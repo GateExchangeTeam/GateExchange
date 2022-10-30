93b63a1e924eb44743cfac80bf1360936a4b2bb5
joycezhang <joycejiayizhang@gmail.com>
2022-10-30 16:52:49 -0400

    create post model and example seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_30_184645) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "view"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
