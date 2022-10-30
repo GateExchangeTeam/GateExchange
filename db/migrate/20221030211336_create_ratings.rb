0ed0c6a45982a7b6f46771c708f305a6a9e860c8
joycezhang <joycejiayizhang@gmail.com>
2022-10-30 17:17:08 -0400

    generate ratings model attributes and controller

---
+++ db/migrate/20221030211336_create_ratings.rb
@@ -0,0 +1,10 @@
+class CreateRatings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :ratings do |t|
+      t.integer :up
+      t.integer :down
+
+      t.timestamps
+    end
+  end
+end
