39e3ccfd3a471027c76be125d9536e8941f41905
jpaints3 <jopaintsil@gmail.com>
2022-11-10 11:35:31 -0500

    fix: Remove duplicate column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_08_151613) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_08_200114) do
   create_table "comments", force: :cascade do |t|
     t.string "text_body"
     t.datetime "created_at", null: false
@@ -67,6 +67,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_08_151613) do
 
   create_table "tags", force: :cascade do |t|
     t.string "tag_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "passcode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
