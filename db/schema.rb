2fc86ad9fcc972a9d76973c6524271dd8e5fd808
“akamran2001” <akamran@colgate.edu>
2022-11-14 01:55:12 -0500

    Posts show page stylized

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,72 +10,73 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_221_108_200_114) do
-  create_table 'comments', force: :cascade do |t|
-    t.string 'text_body'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'post_id'
-    t.index ['post_id'], name: 'index_comments_on_post_id'
+ActiveRecord::Schema[7.0].define(version: 2022_11_08_200114) do
+  create_table "comments", force: :cascade do |t|
+    t.string "text_body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
-  create_table 'courses', force: :cascade do |t|
-    t.string 'title'
-    t.integer 'course_code'
-    t.text 'description'
-    t.string 'department'
-    t.string 'faculty'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.integer "course_code"
+    t.text "description"
+    t.string "department"
+    t.string "faculty"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'nested_comments', force: :cascade do |t|
-    t.string 'text'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'comment_id'
-    t.index ['comment_id'], name: 'index_nested_comments_on_comment_id'
+  create_table "nested_comments", force: :cascade do |t|
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "comment_id"
+    t.index ["comment_id"], name: "index_nested_comments_on_comment_id"
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.string 'title'
-    t.string 'description'
-    t.integer 'view'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'course_id'
-    t.index ['course_id'], name: 'index_posts_on_course_id'
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "view"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "course_id"
+    t.index ["course_id"], name: "index_posts_on_course_id"
   end
 
-  create_table 'posts_tags', id: false, force: :cascade do |t|
-    t.integer 'posts_id'
-    t.integer 'tags_id'
-    t.index ['posts_id'], name: 'index_posts_tags_on_posts_id'
-    t.index ['tags_id'], name: 'index_posts_tags_on_tags_id'
+  create_table "posts_tags", id: false, force: :cascade do |t|
+    t.integer "posts_id"
+    t.integer "tags_id"
+    t.index ["posts_id"], name: "index_posts_tags_on_posts_id"
+    t.index ["tags_id"], name: "index_posts_tags_on_tags_id"
   end
 
-  create_table 'ratings', force: :cascade do |t|
-    t.integer 'up'
-    t.integer 'down'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'post_id'
-    t.integer 'comment_id'
-    t.index ['comment_id'], name: 'index_ratings_on_comment_id'
-    t.index ['post_id'], name: 'index_ratings_on_post_id'
+  create_table "ratings", force: :cascade do |t|
+    t.integer "up"
+    t.integer "down"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.integer "comment_id"
+    t.index ["comment_id"], name: "index_ratings_on_comment_id"
+    t.index ["post_id"], name: "index_ratings_on_post_id"
   end
 
-  create_table 'tags', force: :cascade do |t|
-    t.string 'tag_name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "tags", force: :cascade do |t|
+    t.string "tag_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.string 'passcode'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "passcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
