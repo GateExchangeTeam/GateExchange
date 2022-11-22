7ee8e94e6e9d302095372b9df786cd86ff03d013
“akamran2001” <akamran@colgate.edu>
2022-11-22 17:34:57 -0500

    Comment validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-  validates :text_body, length: { minimum: 5 }, allow_nil: false
+  validates :text_body, length: { minimum: 5 }, allow_nil: false, presence: true
   belongs_to :post
   has_many :ratings
   has_many :nested_comments
