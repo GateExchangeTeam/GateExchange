cc8e650490f80e11a3a23572e6ebc9d577d698bd
“akamran2001” <akamran@colgate.edu>
2022-11-22 15:06:33 -0500

    new post page style changes

--- app/javascript/courses.js
+++ app/javascript/courses.js
@@ -1,12 +1,22 @@
-let palette = $(".palette");
+function animateCourses(){
+    let urlParams = new URLSearchParams(window.location.search);
+    if(urlParams.has("animate")){
+        let palette = $(".palette");
 
-palette.each(function (){
-    $(this).hide();
-});
+        palette.each(function (){
+            $(this).hide();
+        });
 
-palette.each(function (index){
-    let pal = $(this);
-    setTimeout(function (){
-        pal.fadeIn(400);
-    },100+(index*350))
-});
+        palette.each(function (index){
+            let pal = $(this);
+            setTimeout(function (){
+                pal.fadeIn(400);
+            },100+(index*350))
+        });
+    }
+}
+document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+       animateCourses();
+    }
+};
