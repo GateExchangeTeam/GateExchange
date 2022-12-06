4f8370908e9051da0e5e782dd7c8fdfe52c8dde5
“akamran2001” <akamran@colgate.edu>
2022-12-06 15:44:37 -0500

    rails UJS setup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,10 +3,12 @@ import "trix"
 import "@rails/actiontext"
 import "trix-editor-overrides"
 import LocalTime from "local-time"
+import Rails from '@rails/ujs';
 
+Rails.start();
 LocalTime.start()
 
-$(function() {
+jquery(function() {
     setTimeout(function(){
         $('.flash-messages').slideUp(500); // Remove flash messages after a second
     }, 1000);
