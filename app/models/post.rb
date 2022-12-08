048fd76c3a0a89f51521c10f54066fd9f4d17128
“akamran2001” <akamran@colgate.edu>
2022-12-08 03:51:25 -0500

    clean up db, associate posts with users, use gem for fake data

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,9 @@
 
 class Post < ApplicationRecord
   belongs_to :course
-  has_many :comments, as: :commentable
-  has_many :ratings, as: :rateable
+  belongs_to :user
+  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :ratings, as: :rateable, dependent: :destroy
   has_rich_text :content
   validates :title, length: { minimum: 1 }, presence: true
   validates :description, length: { minimum: 1 }, presence: true
