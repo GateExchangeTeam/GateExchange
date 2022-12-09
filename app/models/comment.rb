2b8ca092011f7af27d9b19afeb8d5d73e4354938
“akamran2001” <akamran@colgate.edu>
2022-12-09 11:47:53 -0500

    comment does not relate to non existing rating model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,6 @@
 class Comment < ApplicationRecord
   acts_as_votable
   validates :text_body, length: { minimum: 5 }, allow_nil: false, presence: true
-  has_many :ratings, as: :rateable
   belongs_to :commentable, polymorphic: true
   belongs_to :user
   has_many :comments, as: :commentable
