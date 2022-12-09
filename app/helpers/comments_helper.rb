3677112f46a3db4acb3d7f55fd5679e5a80ea09a
“akamran2001” <akamran@colgate.edu>
2022-12-09 11:46:12 -0500

    bug fixes for comment sorting and post deletion

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -18,9 +18,9 @@ module CommentsHelper
       sort = params[:sort]
       @comments = case sort
                   when 'likes'
-                    commentable.comments.all.left_joins(:ratings).group(:id).order('SUM(ratings.up) DESC')
+                    commentable.comments.order(cached_votes_up: :desc)
                   when 'dislikes'
-                    commentable.comments.all.left_joins(:ratings).group(:id).order('SUM(ratings.down) DESC')
+                    commentable.comments.all.order(cached_votes_down: :desc)
                   when 'newest'
                     commentable.comments.all.order('updated_at DESC')
                   else
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,6 @@ class Post < ApplicationRecord
   belongs_to :course
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-  has_many :ratings, as: :rateable, dependent: :destroy
   has_rich_text :content
   validates :title, length: { minimum: 1 }, presence: true
   validates :description, length: { minimum: 1 }, presence: true
